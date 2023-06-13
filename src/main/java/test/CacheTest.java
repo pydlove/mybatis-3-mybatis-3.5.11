fd0ec44ecc96f3b733363e9668e62d6880a4a1a9
aiocloud@126.com <py1149050048>
2023-06-13 19:49:09 +0800

    init

---
+++ src/main/java/test/CacheTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CacheTest {
+
+  public static void main(String[] args) throws IOException {
+    String resource = "mybatis-config.xml";
+    InputStream inputStream = Resources.getResourceAsStream(resource);
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+  }
+}
